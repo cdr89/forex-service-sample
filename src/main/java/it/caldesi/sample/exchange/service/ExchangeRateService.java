7458b40b4f7cf7c7dc97b7f09ac4fd8fb45bd297
Domenico <d.r.caldesi@gmail.com>
2017-05-06 18:06:10 +0200

    added ExchangeRateService to manage ExchangeRate entities

---
+++ src/main/java/it/caldesi/sample/exchange/service/ExchangeRateService.java
@@ -0,0 +1,41 @@
+package it.caldesi.sample.exchange.service;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.caldesi.sample.exchange.data.domain.ExchangeRate;
+import it.caldesi.sample.exchange.data.repository.ExchangeRateRepository;
+
+@Service
+public class ExchangeRateService {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private EntityManager entityManager;
+
+	@Autowired
+	private ExchangeRateRepository exchangeRateRepository;
+
+	@Transactional
+	public void updateRates(Collection<ExchangeRate> rates) {
+		log.debug("Deleting all rates");
+		exchangeRateRepository.findAll().forEach(rate -> exchangeRateRepository.delete(rate));
+		entityManager.flush();
+		
+		log.debug("Writing new rates");
+		rates.stream().forEach(rate -> entityManager.persist(rate));
+	}
+
+	public Iterable<ExchangeRate> getAllRates() {
+		return exchangeRateRepository.findAll();
+	}
+
+}
